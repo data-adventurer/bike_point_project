777d6d29d9ced0f105ed00baea93ec3c5cd5821f
Lorraine Ferrusi <lferrusi94@gmail.com>
2025-11-05 15:31:34 +0000

    Added extract timestamp and exported json to local drive

--- exrtact_bike_point_api.py
+++ exrtact_bike_point_api.py
@@ -1,10 +1,28 @@
 from datetime import datetime
 import time as t
 import requests
+import json
 
 url = 'https://api.tfl.gov.uk/BikePoint'
 
 response = requests.get(url, timeout=10)
 
 print(response.status_code)
+
+data = response.json()
+
+# print(data)
+
+extract_time =  datetime.now()
+
+for bp in data:
+    bp['extract_time'] = str(extract_time)
+
+# print(data[0])
+
+filepath = 'data/' +  extract_time.strftime('%Y-%m-%dT%H-%M-%S') + '.json'
+
+with open(filepath, 'w') as file:
+    json.dump(data, file)
+
 
